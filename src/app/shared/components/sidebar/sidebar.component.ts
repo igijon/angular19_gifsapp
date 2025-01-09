855dc400774deec099fa6175ec506bdd185eb6de
Inma Gijón <igijoninf@gmail.com>
2025-01-09 17:42:03 +0100

    Estructura inicial del proyecto

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  standalone: false
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
