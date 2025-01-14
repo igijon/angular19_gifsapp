3ae48d940a0734dcb9aed9e2723cb3f6cda12af0
Inma Gijón <igijoninf@gmail.com>
2025-01-14 13:09:06 +0100

    Peticiones HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,9 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
   private giphy_api_key: string = 'UJhtmqNexoXg8nxMJB20TFS5xltag48Z';  
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory]; //Copia por seguridad. Operador spread
@@ -33,6 +34,17 @@ export class GifsService {
     if ( tag.length === 0 ) return;
     this.organizeHistory(tag);
     
-    
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=UJhtmqNexoXg8nxMJB20TFS5xltag48Z&q=valorant&limit=10')
+    //   .then( resp => resp.json() )
+    //   .then( data => console.log(data) );
+    //Observable: objeto que a lo largo del tiempo podemos hacer que emita valores. Si nos suscribimos a un observable
+    //somos alertados cuando ese objeto emite valores.
+    const params = new HttpParams()
+      .set('api_key', this.giphy_api_key)
+      .set('limit', 10)
+      .set('q', tag)
+    this.http.get( `${this.serviceUrl}/search`, {params} )
+      .subscribe( resp => console.log(resp) );
+      
   } 
 }
